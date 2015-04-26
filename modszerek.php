31689fad8b9d39b8c8e92d589ed67fd5f5b6991f
TomVarga <tom@tvarga.hu>
2015-04-27 00:12:05 +0200

    add link to szakdolgozat on modszerek.php

--- modszerek.php
+++ modszerek.php
@@ -24,12 +24,8 @@
 
 			<h2> Módszerek &rsaquo;</h2>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
+				Részletes leírás <a href="https://github.com/TomVarga/borkostolas_szakdolgozat/blob/master/VargaTamas.pdf" class="link">ebben</a> a szakdolgozatban található, melynek feladata volt a web és mobil
+				alkalmazás elkészítése.<br/>
 			</p>
 		</div>
 	</div>
